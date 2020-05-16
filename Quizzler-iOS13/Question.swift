ab703d9662eec848698a4ce651efb278da9605e1
Kelley Chaplain <kmchaplain@gmail.com>
2020-05-16 15:10:55 -0500

    created question struct to make code more dynamic

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Kelley Chaplain on 5/16/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
