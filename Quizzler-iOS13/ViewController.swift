13be423ec393e94bd969468a6c8b57394ec14220
Kelley Chaplain <kmchaplain@gmail.com>
2020-05-16 14:18:02 -0500

    created array with questions and an update function to help rotate through questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,15 +15,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+        questionNumber += 1
     }
     
-
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 }
 
